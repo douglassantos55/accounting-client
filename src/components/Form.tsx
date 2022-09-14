1fc4f3a56dc48f9daeb0573b54091ae11ca51a1a
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-09-13 22:10:14 -0300

    refactor: form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,55 @@
+import { Accessor, Component, createContext, createSignal, ParentProps, useContext } from "solid-js";
+
+type Form = {
+    errors: Accessor<string[]>;
+    data: Accessor<Record<string, any>>;
+    handleChange: (evt: InputEvent) => void;
+}
+
+type FormProps = {
+    initialData: Record<string, any>;
+    handleSubmit: (data: Record<string, any>) => Promise<void>;
+}
+
+const FormContext = createContext<Form>();
+
+export const Form: Component<ParentProps<FormProps>> = (props: any) => {
+    const [data, setData] = createSignal(props.initialData);
+    const [errors, setErrors] = createSignal<string[]>([]);
+
+    function handleChange(evt: InputEvent) {
+        const input = evt.target as HTMLInputElement
+        setData(prev => ({
+            ...prev,
+            [input.name]: input.value,
+        }))
+    }
+
+    async function handleSubmit(evt: SubmitEvent) {
+        evt.preventDefault();
+        try {
+            setErrors([]);
+            await props.handleSubmit(data());
+        } catch (err) {
+            setErrors(prev => ([...prev, (err as Error).message]));
+        }
+    }
+
+    const value: Form = {
+        data,
+        errors,
+        handleChange,
+    }
+
+    return (
+        <FormContext.Provider value={value}>
+            <form onSubmit={handleSubmit}>
+                {props.children}
+            </form>
+        </FormContext.Provider>
+    );
+}
+
+export function useForm() {
+    return useContext(FormContext) as Form;
+}
--- src/pages/accounts/Form.tsx
+++ src/pages/accounts/Form.tsx
@@ -1,62 +1,77 @@
+import { useNavigate } from "@solidjs/router";
 import axios from "axios";
-import { Component, createSignal, Index } from "solid-js";
-import { AccountType } from "../../types";
+import { Component, createSignal, Index, For, onMount } from "solid-js";
+import { Form, useForm } from "../../components/Form";
+import { Account, AccountType } from "../../types";
 
 const TYPES = Object.values(AccountType).filter(v => typeof v !== "number");
 
-function useForm<T>(initialData: T) {
-    const [data, setData] = createSignal<T>(initialData);
-
-    function handleChange(evt: InputEvent) {
-        const input = evt.target as HTMLInputElement
-        setData((prev: T) => ({
-            ...prev,
-            [input.name]: input.value,
-        }))
-    }
-
-    return { data, handleChange };
-}
-
-const Form: Component = () => {
-    const { data, handleChange } = useForm({
-        name: "",
-        type: "",
-        parent_id: "",
-    });
-
-    async function createAccount(evt: SubmitEvent) {
-        evt.preventDefault();
-        await axios.post("http://localhost:8080/accounts", {
-            name: data().name,
-            type: parseInt(data().type),
-            parent_id: data().parent_id ? parseInt(data().parent_id) : null,
-        }, {
-            headers: { "CompanyID": 1 },
-        });
-    }
+const AccountForm: Component<{ accounts: Account[] }> = (props) => {
+    const { data, errors, handleChange } = useForm();
 
     return (
         <div class="container">
             <h1>Create an account</h1>
 
-            <form onSubmit={createAccount}>
-                <div class="mb-3">
-                    <input name="name" class="form-control" onInput={handleChange} />
-                </div>
+            <For each={errors()}>{error => <p>{error}</p>}</For>
 
-                <div class="mb-3">
-                    <select name="type" class="form-control" onInput={handleChange}>
-                        <Index each={TYPES}>{(type, idx) =>
-                            <option value={idx}>{type}</option>
-                        }</Index>
-                    </select>
-                </div>
+            <div class="mb-3">
+                <input name="name" class="form-control" value={data().name} onInput={handleChange} />
+            </div>
 
-                <button type="submit" class="btn btn-primary">Create</button>
-            </form>
+            <div class="mb-3">
+                <select name="type" class="form-control" value={data().type} onInput={handleChange}>
+                    <option value="">Select an option</option>
+                    <Index each={TYPES}>{(type, idx) =>
+                        <option value={idx}>{type}</option>
+                    }</Index>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <select name="parent_id" class="form-control" value={data().parent_id} onInput={handleChange}>
+                    <option value="">Select an option</option>
+                    <For each={props.accounts}>{account =>
+                        <option value={account.ID}>{account.name}</option>
+                    }</For>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Create</button>
         </div>
     );
 }
 
-export default Form;
+export default function() {
+    const navigate = useNavigate();
+    const [accounts, setAccounts] = createSignal<Account[]>([]);
+
+    onMount(async () => {
+        const res = await axios.get("http://localhost:8080/accounts", {
+            headers: { CompanyID: 1 },
+        })
+        setAccounts(res.data);
+    })
+
+    async function createAccount(data: Record<string, any>) {
+        await axios.post("http://localhost:8080/accounts", {
+            name: data.name,
+            type: parseInt(data.type),
+            parent_id: data.parent_id ? parseInt(data.parent_id) : null,
+        }, { headers: { CompanyID: 1 } });
+
+        navigate("/accounts");
+    }
+
+    const initialData = {
+        name: '',
+        type: '',
+        parent_id: '',
+    };
+
+    return (
+        <Form handleSubmit={createAccount} initialData={initialData}>
+            <AccountForm accounts={accounts()} />
+        </Form>
+    );
+}
