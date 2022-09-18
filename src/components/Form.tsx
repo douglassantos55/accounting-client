cfe324321d4763a7a7fe0d16b082b5c1d658ed42
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-09-18 17:31:32 -0300

    refactor: form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,7 @@
-import { Accessor, Component, createContext, createSignal, ParentProps, useContext } from "solid-js";
-
-type Error = {
-    error: string;
-}
+import { Accessor, Component, createContext, createMemo, createSignal, ParentProps, Show, useContext } from "solid-js";
 
 type Form = {
-    errors: Accessor<string[]>;
+    errors: Accessor<Record<string, string>>;
     data: Accessor<Record<string, any>>;
     handleChange: (evt: InputEvent) => void;
 }
@@ -17,9 +13,48 @@ type FormProps = {
 
 const FormContext = createContext<Form>();
 
+type FieldProps = {
+    name: string;
+    label?: string;
+}
+
+export const Field: Component<FieldProps> = function(props) {
+    const { errors } = useForm();
+
+    const error = createMemo(function() {
+        return errors()[props.name];
+    });
+
+    return (
+        <div class="mb-4">
+            <Show when={props.label}>
+                <label for={props.name} class="form-label">{props.label}</label>
+            </Show>
+
+            <Input id={props.name} name={props.name} classList={{ 'is-invalid': error() }} />
+
+            <Show when={error()}>
+                <div class="invalid-feedback">{error()}</div>
+            </Show>
+        </div>
+    );
+}
+
+export const Input: Component<{ name: string;[K: string]: any }> = function(props) {
+    const { data, handleChange } = useForm();
+    return (
+        <input
+            {...props}
+            class="form-control"
+            onInput={handleChange}
+            value={data()[props.name]}
+        />
+    );
+}
+
 export const Form: Component<ParentProps<FormProps>> = (props: any) => {
     const [data, setData] = createSignal(props.initialData);
-    const [errors, setErrors] = createSignal<string[]>([]);
+    const [errors, setErrors] = createSignal<Record<string, string>>({});
 
     function handleChange(evt: InputEvent) {
         const input = evt.target as HTMLInputElement
@@ -32,10 +67,10 @@ export const Form: Component<ParentProps<FormProps>> = (props: any) => {
     async function handleSubmit(evt: SubmitEvent) {
         evt.preventDefault();
         try {
-            setErrors([]);
+            setErrors({});
             await props.handleSubmit(data());
         } catch (err) {
-            setErrors(prev => ([...prev, (err as Error).error]));
+            setErrors(prev => ({ ...prev, ...(err as Record<string, string>) }));
         }
     }
 
